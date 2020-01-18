b251960072b3d03586b153070fda2a3ede343de6
hosuseri <mercury@hosuseri.com>
2020-01-19 00:40:50 +0900

    Overall reviewed. Tuned a little.

--- asmapp10/context.asm
+++ asmapp10/context.asm
@@ -9,151 +9,144 @@
 ;;; SP[1] = return(L)
 ;;; 
 save_context:
-	std	z + c_r0, r0
-	in	r0, SREG
-	std	z + c_sreg, r0
+	std	z + c_r26, r26
+	in	r26, SREG
+	std	z + c_sreg, r26
+	std	z + c_r27, r27
 	std	z + c_r28, r28
 	std	z + c_r29, r29
 	;; return
-	pop	r28
 	pop	r29
+	pop	r28
 	;; continue
-	pop	r0
-	std	z + c_pcl, r0
-	pop	r0
-	std	z + c_pch, r0
-	;; r30
-	pop	r0
-	std	z + c_r30, r0
-	;; r31
-	pop	r0
-	std	z + c_r31, r0
+	pop	r26
+	pop	r27
+	std	z + c_pcl, r26
+	std	z + c_pch, r27
+	;; r31:r30
+	pop	r27
+	pop	r26
+	std	z + c_r30, r26
+	std	z + c_r31, r27
 	;;
 	cli
-	in	r0, SPL
-	std	z + c_spl, r0
-	in	r0, SPH
-	std	z + c_sph, r0
+	in	r26, SPL
+	in	r27, SPH
 	;; restore the return
-	push	r29
 	push	r28
+	push	r29
 	;;
 save_gregs:
-	ld	r0, z
-	st	z+, r0
-	st	z+, r1
-	st	z+, r2
-	st	z+, r3
-	st	z+, r4
-	st	z+, r5
-	st	z+, r6
-	st	z+, r7
-	st	z+, r8
-	st	z+, r9
-	st	z+, r10
-	st	z+, r11
-	st	z+, r12
-	st	z+, r13
-	st	z+, r14
-	st	z+, r15
-	st	z+, r16
-	st	z+, r17
-	st	z+, r18
-	st	z+, r19
-	st	z+, r20
-	st	z+, r21
-	st	z+, r22
-	st	z+, r23
-	st	z+, r24
-	st	z+, r25
-	st	z+, r26
-	st	z+, r27
+	movw	r26, r30
+	st	x+, r0
+	st	x+, r1
+	st	x+, r2
+	st	x+, r3
+	st	x+, r4
+	st	x+, r5
+	st	x+, r6
+	st	x+, r7
+	st	x+, r8
+	st	x+, r9
+	st	x+, r10
+	st	x+, r11
+	st	x+, r12
+	st	x+, r13
+	st	x+, r14
+	st	x+, r15
+	st	x+, r16
+	st	x+, r17
+	st	x+, r18
+	st	x+, r19
+	st	x+, r20
+	st	x+, r21
+	st	x+, r22
+	st	x+, r23
+	st	x+, r24
+	st	x+, r25
 	ret
 ;;; 
 init_context:
-	std	z + c_r0, r0
-	in	r0, SREG
-	std	z + c_sreg, r0
+	std	z + c_r26, r26
+	in	r26, SREG
+	std	z + c_sreg, r26
+	std	z + c_r27, r27
 	std	z + c_r28, r28
 	std	z + c_r29, r29
 	;; return
-	pop	r28
 	pop	r29
+	pop	r28
 	;; continue
-	pop	r0
-	std	z + c_pcl, r0
-	pop	r0
-	std	z + c_pch, r0
-	;; r30
-	pop	r0
-	std	z + c_r30, r0
-	;; r31
-	pop	r0
-	std	z + c_r31, r0
-	;;
+	pop	r26
+	pop	r27
+	std	z + c_pcl, r26
+	std	z + c_pch, r27
+	;; r31:r30
+	pop	r27
+	pop	r26
+	std	z + c_r30, r26
+	std	z + c_r31, r27
 	;; restore the return
-	push	r29
 	push	r28
+	push	r29
 	;;
 	rjmp	save_gregs
 
 ;;;
+;;; jmp to this, not call.
 ;;; r31:r30 = context
 ;;;
 restore_context:
+	movw	r26, r30
+	ld	r0, x+
+	ld	r1, x+
+	ld	r2, x+
+	ld	r3, x+
+	ld	r4, x+
+	ld	r5, x+
+	ld	r6, x+
+	ld	r7, x+
+	ld	r8, x+
+	ld	r9, x+
+	ld	r10, x+
+	ld	r11, x+
+	ld	r12, x+
+	ld	r13, x+
+	ld	r14, x+
+	ld	r15, x+
+	ld	r16, x+
+	ld	r17, x+
+	ld	r18, x+
+	ld	r19, x+
+	ld	r20, x+
+	ld	r21, x+
+	ld	r22, x+
+	ld	r23, x+
+	ld	r24, x+
+	ld	r25, x+
+	ld	r28, x+
+	ld	r29, x+
+	movw	r26, r28
+	cli
 	ldd	r28, z + c_spl
 	ldd	r29, z + c_sph
-	cli
 	out	SPL, r28
 	out	SPH, r29
-	;;
 	;; continue
 	ldd	r28, z + c_pcl
 	ldd	r29, z + c_pch
-	;; what is happening? Big endian on the stack?
 	push	r28
 	push	r29
-	;; r30, r31
+	;;
 	ldd	r28, z + c_r30
 	ldd	r29, z + c_r31
-	push	r29
 	push	r28
+	push	r29
 	;; SREG
 	ldd	r28, z + c_sreg
-	push	r28
-	;;
-	ld	r0, z+
-	ld	r1, z+
-	ld	r2, z+
-	ld	r3, z+
-	ld	r4, z+
-	ld	r5, z+
-	ld	r6, z+
-	ld	r7, z+
-	ld	r8, z+
-	ld	r9, z+
-	ld	r10, z+
-	ld	r11, z+
-	ld	r12, z+
-	ld	r13, z+
-	ld	r14, z+
-	ld	r15, z+
-	ld	r16, z+
-	ld	r17, z+
-	ld	r18, z+
-	ld	r19, z+
-	ld	r20, z+
-	ld	r21, z+
-	ld	r22, z+
-	ld	r23, z+
-	ld	r24, z+
-	ld	r25, z+
-	ld	r26, z+
-	ld	r27, z+
-	ld	r28, z+
-	ld	r29, z+
-	pop	r30
-	out	SREG, r30
+	out	SREG, r28
+	ldd	r28, z + c_r28
+	ldd	r29, z + c_r29
 	pop	r30
 	pop	r31
 	ret
--- asmapp10/main.asm
+++ asmapp10/main.asm
@@ -17,71 +17,71 @@
 #define RAMSIZE (RAMEND + 1 - RAMSTART)
 
 	.CSEG
-	.ORG 0
-	jmp start
-	.ORG 0x001c
-	jmp timer0_match
+	.ORG	0
+	jmp	start
+	.ORG	0x001c
+	jmp	timer0_match
 ;;;
 	.ORG 0x0034
 start:
-	clr r2
-	mov r3, r2
-	inc r3
+	clr	r2
+	mov	r3, r2
+	inc	r3
 	;;
-	ldi r30, LOW(RAMSTART)
-	ldi r31, HIGH(RAMSTART)
-	ldi r16, LOW(RAMSIZE)
-	ldi r17, HIGH(RAMSIZE)
+	ldi	r30, LOW(RAMSTART)
+	ldi	r31, HIGH(RAMSTART)
+	ldi	r16, LOW(RAMSIZE)
+	ldi	r17, HIGH(RAMSIZE)
 ramclr:
-	st x+, r2
-	sub r16, r3
-	sbc r17, r2
-	mov r0, r16
-	or r0, r17
-	brne ramclr
+	st	x+, r2
+	sub	r16, r3
+	sbc	r17, r2
+	mov	r0, r16
+	or	r0, r17
+	brne	ramclr
 	;; 
 	;; disable SPI
-	out SPCR, r2
+	out	SPCR, r2
 	;; 
 	;; setting-up clock pre-scaler
-	ldi r26, LOW(CLKPR)
-	ldi r27, HIGH(CLKPR)
-	clr r0
+	ldi	r26, LOW(CLKPR)
+	ldi	r27, HIGH(CLKPR)
+	clr	r0
 	set
-	bld r0, CLKPCE
-	st x, r0
-	st x, r2
+	bld	r0, CLKPCE
+	st	x, r0
+	st	x, r2
 	;; 
 	;; setting-up PORTB direction
-	clr r0
+	clr	r0
 	set
-	bld r0, DDB5
-	out DDRB, r0
+	bld	r0, DDB5
+	out	DDRB, r0
 	;;
 	;; setting-up PORTC direction
-	ldi r16, 0x0f
-	out DDRC, r16
-	out PORTC, r2
+	ldi	r16, 0x0f
+	out	DDRC, r16
+	out	PORTC, r2
 	;;
 	;; setting-up TIMER0
-	ldi r16, (0<<COM0A1) | (0<<COM0A0) | (1<<WGM01) | (0<<WGM00)
-	out TCCR0A, r16
-	ldi r16, (0<<WGM02) | (1<<CS02) | (0<<CS01) | (0<<CS00)
-	out TCCR0B, r16
-	ldi r16, 78
-	out OCR0A, r16
+	ldi	r16, (0<<COM0A1) | (0<<COM0A0) | (1<<WGM01) | (0<<WGM00)
+	out	TCCR0A, r16
+	ldi	r16, (0<<WGM02) | (1<<CS02) | (0<<CS01) | (0<<CS00)
+	out	TCCR0B, r16
+	ldi	r16, 78
+	out	OCR0A, r16
 	;;
-	ldi r26, LOW(TIMSK0)
-	ldi r27, HIGH(TIMSK0)
-	clr r0
+	ldi	r26, LOW(TIMSK0)
+	ldi	r27, HIGH(TIMSK0)
+	clr	r0
 	set
-	bld r0, OCIE0A
-	st x, r0
+	bld	r0, OCIE0A
+	st	x, r0
 	;;
 	;;
 	;; Z has systicks
-	ldi r30, LOW(systicks)
-	ldi r31, HIGH(systicks)
+	ldi	r30, LOW(systicks)
+	ldi	r31, HIGH(systicks)
 ;;;
 #if 0	/* for testing */
 	ldi	r16, 0x71
@@ -109,8 +109,6 @@ ramclr:
 	ldi	r30, LOW(context0)
 	ldi	r31, HIGH(context0)
 	call	init_context
-	ldi	r30, LOW(context0)
-	ldi	r31, HIGH(context0)
 	in	r0, SPL
 	std	z + c_spl, r0
 	in	r0, SPH
@@ -132,8 +130,6 @@ ramclr:
 	ldi	r30, LOW(context1)
 	ldi	r31, HIGH(context1)
 	call	init_context
-	ldi	r30, LOW(context1)
-	ldi	r31, HIGH(context1)
 	ldi	r16, LOW(stack1)
 	std	z + c_spl, r16
 	ldi	r16, HIGH(stack1)
@@ -149,15 +145,13 @@ ramclr:
 	push	r30
 	ldi	r24, 8
 	mov	r25, r2
-	ldi	r30, LOW(coroutine2)
 	ldi	r31, HIGH(coroutine2)
+	ldi	r30, LOW(coroutine2)
 	push	r31
 	push	r30
 	ldi	r30, LOW(context2)
 	ldi	r31, HIGH(context2)
 	call	init_context
-	ldi	r30, LOW(context2)
-	ldi	r31, HIGH(context2)
 	ldi	r16, LOW(stack2)
 	std	z + c_spl, r16
 	ldi	r16, HIGH(stack2)
@@ -277,69 +271,69 @@ wait_edge_20:
 ;;;
 ;;;
 timer0_match:
-	push r0
-	in r0, SREG
-	push r0
-	push r1
-	push r16
-	push r31
-	push r30
-	ldi r30, LOW(systicks)
-	ldi r31, HIGH(systicks)
+	push	r0
+	in	r0, SREG
+	push	r0
+	push	r1
+	push	r16
+	push	r31
+	push	r30
+	ldi	r30, LOW(systicks)
+	ldi	r31, HIGH(systicks)
 	;;
 timer0_match10:
-	ld r0, z
-	add r0, r3
-	st z+, r0
-	ld r0, z
-	adc r0, r2
-	st z+, r0
-	ld r0, z
-	adc r0, r2
-	st z+, r0
-	ld r0, z
-	adc r0, r2
-	st z+, r0
-	ld r0, z
-	adc r0, r2
-	st z+, r0
-	ld r0, z
-	adc r0, r2
-	st z+, r0
+	ld	r0, z
+	add	r0, r3
+	st	z+, r0
+	ld	r0, z
+	adc	r0, r2
+	st	z+, r0
+	ld	r0, z
+	adc	r0, r2
+	st	z+, r0
+	ld	r0, z
+	adc	r0, r2
+	st	z+, r0
+	ld	r0, z
+	adc	r0, r2
+	st	z+, r0
+	ld	r0, z
+	adc	r0, r2
+	st	z+, r0
 	;;
-	ld r0, z
-	ldi r16, 66
-	add r0, r16
-	st z+, r0
-	ld r0, z
-	adc r0, r2
-	st z+, r0
-	brcc timer0_match_30
+	ld	r0, z
+	ldi	r16, 66
+	add	r0, r16
+	st	z+, r0
+	ld	r0, z
+	adc	r0, r2
+	st	z+, r0
+	brcc	timer0_match_30
 	;;
-	ld r0, z
-	add r0, r3
-	st z, r0
-	ldd r0, z+1
-	adc r0, r2
-	std z+1, r0
-	ldd r0, z+2
-	adc r0, r2
-	std z+2, r0
+	ld	r0, z
+	add	r0, r3
+	st	z, r0
+	ldd	r0, z+1
+	adc	r0, r2
+	std	z+1, r0
+	ldd	r0, z+2
+	adc	r0, r2
+	std	z+2, r0
 	;;
-	ldd r0, z+3
-	ldi r16, 1<<DDB5
-	eor r0, r16
-	std z+3, r0
-	out PORTB, r0
+	ldd	r0, z+3
+	ldi	r16, 1<<DDB5
+	eor	r0, r16
+	std	z+3, r0
+	out	PORTB, r0
 	;;
 timer0_match_30:
-	pop r30
-	pop r31
-	pop r16
-	pop r1
-	pop r0
-	out SREG, r0
-	pop r0
+	pop	r30
+	pop	r31
+	pop	r16
+	pop	r1
+	pop	r0
+	out	SREG, r0
+	pop	r0
 	reti
 	;;
 	;; R8 / R4
@@ -347,18 +341,18 @@ timer0_match_30:
 	;; R0 <= remainder
 	;; breaks: R16
 udiv0808:
-	clr r0
-	ldi r16, 8
+	clr	r0
+	ldi	r16, 8
 udiv0808_10:
-	lsl r8
-	rol r0
-	cp r0, r4
-	brcs udiv0808_20
-	inc r8
-	sub r0, r4
+	lsl	r8
+	rol	r0
+	cp	r0, r4
+	brcs	udiv0808_20
+	inc	r8
+	sub	r0, r4
 udiv0808_20:
-	dec r16
-	brne udiv0808_10
+	dec	r16
+	brne	udiv0808_10
 	ret
 	;;
 	;; R9:R8 / R4
@@ -366,19 +360,19 @@ udiv0808_20:
 	;; R0    <= remainder
 	;; breaks: R16
 udiv1608:
-	clr r0
-	ldi r16, 16
+	clr	r0
+	ldi	r16, 16
 udiv1608_10:
-	lsl r8
-	rol r9
-	rol r0
-	cp r0, r4
-	brcs udiv1608_20
-	inc r8
-	sub r0, r4
+	lsl	r8
+	rol	r9
+	rol	r0
+	cp	r0, r4
+	brcs	udiv1608_20
+	inc	r8
+	sub	r0, r4
 udiv1608_20:
-	dec r16
-	brne udiv1608_10
+	dec	r16
+	brne	udiv1608_10
 	ret
 	;;
 	;; R10:R9:R8 / R4
@@ -386,92 +380,92 @@ udiv1608_20:
 	;; R0    <= remainder
 	;; breaks: R16
 udiv2408:
-	clr r0
-	ldi r16, 24
+	clr	r0
+	ldi	r16, 24
 udiv2408_10:
-	lsl r8
-	rol r9
-	rol r10
-	rol r0
-	cp r0, r4
-	brcs udiv2408_20
-	inc r8
-	sub r0, r4
+	lsl	r8
+	rol	r9
+	rol	r10
+	rol	r0
+	cp	r0, r4
+	brcs	udiv2408_20
+	inc	r8
+	sub	r0, r4
 udiv2408_20:
-	dec r16
-	brne udiv2408_10
+	dec	r16
+	brne	udiv2408_10
 	ret
 	;;
 emit_light:
-	push r31
-	push r30
-	push r17
-	push r16
-	push r4
-	push r1
-	push r0
-	ldi r30, LOW(seven_seg<<1)
-	ldi r31, HIGH(seven_seg<<1)
-	add r30, r0
-	adc r31, r2
+	push	r31
+	push	r30
+	push	r17
+	push	r16
+	push	r4
+	push	r1
+	push	r0
+	ldi	r30, LOW(seven_seg<<1)
+	ldi	r31, HIGH(seven_seg<<1)
+	add	r30, r0
+	adc	r31, r2
 	lpm
-	lsl r0
-	rol r1
-	lsl r0
-	rol r1
-	lsl r0
-	rol r1
-	lsl r0
-	rol r1
-	ldi r17, 2
-	ldi r16, 12
+	lsl	r0
+	rol	r1
+	lsl	r0
+	rol	r1
+	lsl	r0
+	rol	r1
+	lsl	r0
+	rol	r1
+	ldi	r17, 2
+	ldi	r16, 12
 emit_light_10:
-	mov r4, r2
-	lsl r0
-	rol r1
-	rol r4
-	out PORTC, r4
-	or r4, r17
-	out PORTC, r4
-	dec r16
-	brne emit_light_10
-	ldi r17, 8
-	out PORTC, r17
-	ldi r17, 4
-	out PORTC, r17
-	out PORTC, r2
+	mov	r4, r2
+	lsl	r0
+	rol	r1
+	rol	r4
+	out	PORTC, r4
+	or	r4, r17
+	out	PORTC, r4
+	dec	r16
+	brne	emit_light_10
+	ldi	r17, 8
+	out	PORTC, r17
+	ldi	r17, 4
+	out	PORTC, r17
+	out	PORTC, r2
 	;; 
-	pop r0
-	pop r1
-	pop r4
-	pop r16
-	pop r17
-	pop r30
-	pop r31
+	pop	r0
+	pop	r1
+	pop	r4
+	pop	r16
+	pop	r17
+	pop	r30
+	pop	r31
 	ret
 ;;;
 	.INCLUDE "context.asm"
 ;;;
 seven_seg:
-	.DB 0x3f, 0x06, 0x5b, 0x4f, 0x66, 0x6d, 0x7d, 0x27
-	.DB 0x7f, 0x6f, 0x77, 0x7c, 0x39, 0x5e, 0x79, 0x71
+	.DB	0x3f, 0x06, 0x5b, 0x4f, 0x66, 0x6d, 0x7d, 0x27
+	.DB	0x7f, 0x6f, 0x77, 0x7c, 0x39, 0x5e, 0x79, 0x71
 ;;;
 	.DSEG
-	.ORG RAMSTART
+	.ORG	RAMSTART
 context0:
-	.BYTE c_size
+	.BYTE	c_size
 context1:
-	.BYTE c_size
+	.BYTE	c_size
 context2:
-	.BYTE c_size
+	.BYTE	c_size
 ;;;
 systicks:
-	.BYTE 6
-	.BYTE 2
+	.BYTE	6
+	.BYTE	2
 seconds:
-	.BYTE 4
+	.BYTE	4
 minsec:
-	.BYTE 6			; sec., min., hour
+	.BYTE	6	; sec., min., hour
 
 	.BYTE	64
 stack2:
